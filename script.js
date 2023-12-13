c699486d1b820ca87c950f1212a2e9a7efd9b44c
Timur <122696788+stevekadmx@users.noreply.github.com>
2023-12-13 14:03:55 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const calendarTable = document.getElementById('calendar');
+    const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+    // Create header row with days of the week
+    const headerRow = calendarTable.createTHead().insertRow();
+    daysOfWeek.forEach(day => {
+        const th = document.createElement('th');
+        th.textContent = day;
+        headerRow.appendChild(th);
+    });
+
+    // Create calendar cells
+    const currentDate = new Date();
+    const currentMonth = currentDate.getMonth();
+    const daysInMonth = new Date(currentDate.getFullYear(), currentMonth + 1, 0).getDate();
+    const firstDayOfMonth = new Date(currentDate.getFullYear(), currentMonth, 1).getDay();
+    let dayCounter = 1;
+
+    for (let i = 0; i < 6; i++) {
+        const row = calendarTable.insertRow();
+
+        for (let j = 0; j < 7; j++) {
+            const cell = row.insertCell();
+
+            if ((i === 0 && j < firstDayOfMonth) || dayCounter > daysInMonth) {
+                // Empty cells before the first day and after the last day
+                cell.textContent = '';
+            } else {
+                cell.textContent = dayCounter;
+                dayCounter++;
+
+                // Highlight Saturdays and Sundays
+                if (j === 6) {
+                    cell.classList.add('saturday');
+                } else if (j === 0) {
+                    cell.classList.add('sunday');
+                }
+            }
+        }
+    }
+});
